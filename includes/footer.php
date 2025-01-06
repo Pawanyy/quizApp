c7298c880d322e9a069974ed43ecb47f6d1e3b1e
Pawanyy <pawanyyy01@gmail.com>
2025-01-07 00:24:03 +0530

    added tawk chatbot

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,20 @@
     <div class="container text-center">
         <p class="mb-0">&copy; 2024 Quiz Website. All rights reserved.</p>
     </div>
+    <!--Start of Tawk.to Script-->
+    <script type = "text/javascript" >
+        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/677bfca949e2fd8dfe034109/1igu55uav';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })(); 
+    </script> 
+    <!--End of Tawk.to Script -->
 </footer>
 <style>
 body {
